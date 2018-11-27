4213e76cc6ab1a87847fcfe2a57285f4869ad653
Chandler Moeller <chandlermoeller@gmail.com>
2018-11-27 17:00:45 -0500

    Fixed animal care

--- Staff/AnimalCare.php
+++ Staff/AnimalCare.php
@@ -1,15 +1,18 @@
 <?php
-    session_start();
     $hostname = "academic-mysql.cc.gatech.edu"; /*This is your hostname */
     $username = "cs4400_group53"; /*The user id you use to log in phpmyadmin */
     $password ="Efhjn754"; /* the password for phpmyadmin */
     $database = "cs4400_group53"; /* the name of the database that you wish to fetch data from */
     $ntwk = mysqli_connect($hostname, $username, $password, $database);
-    $query = "SELECT * FROM TreatmentNote";
+    
+    $name = htmlspecialchars($_GET["name"]);
+    $species = str_replace('_', " ", htmlspecialchars($_GET["species"]));
+    $detailsQuery = "SELECT * FROM Animal WHERE (name like '%".$name."%' AND species LIKE '%".$species."%')";
+    $details = mysqli_query($ntwk, $detailsQuery);
+    $details = mysqli_fetch_array($details);
+    $query = "SELECT * FROM TreatmentNote WHERE (animal LIKE '%".$name."%' AND species LIKE '%".$species."%')";
     $result = mysqli_query($ntwk, $query);
-
-
-}
+    
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,18 +25,15 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <style>
-
 body {
   background-color: rgb(230, 223, 207)
 }
-
 .center {
     margin: auto;
     width: 50%;
     border: 3px solid #73AD21;
     padding: 10px;
 }
-
 .btn {
   margin: auto;
   width: 10%;
@@ -45,11 +45,9 @@ body {
   font-size: auto;
   cursor:pointer;
 }
-
 th:hover{
     cursor: pointer;
 }
-
 </style>
 </head>
 
@@ -60,6 +58,10 @@ th:hover{
 
 <div class="center">
     <div class="container">
+        <a href="StaffHome.php"><button class="btn">Go Home</button></a>
+    </div>
+    <br>
+    <div class="container">
         <div class="row">
             <div class="col-sm-2">
                 <h4 id="name"></h4>
@@ -68,22 +70,28 @@ th:hover{
                 <h4 id="species"></h4>
             </div>
             <div class="col-sm-2">
-                <h5>Age: </h5>
+                <?php 
+                    echo '<h4>Age: ' . $details['age'] . ' months';
+                ?>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-2">
-                <h5>Exhibit: </h5>
+                <?php 
+                    echo '<h4>Exhibit: ' . $details['exhibit'];
+                ?>
             </div>
             <div class="col-sm-2">
-                <h5>Type: </h5>
+                <?php 
+                    echo '<h4>Type: ' . $details['animalType'];
+                ?>
             </div>
         </div>
 
         <div class="row">
             <div class="form-inline" method="post" action="AnimalCare.php">
                 <textarea name="msg" rows="4" cols="50" maxlength="200"></textarea>
-                <input class="btn" type="submit" name="search" value="Log Notes">
+                <input class="btn" type="submit" name="log" value="Log Notes">
             </div>
         </div>
         
@@ -98,8 +106,8 @@ th:hover{
                 <?php while($row = mysqli_fetch_array($result)) {?>
                 <tr>
                     <td><?php echo $row['staff']; ?></td>
-                    <td><?php echo $row['msg']; ?></td>
-                    <td><?php echo $row['time']; ?></td>
+                    <td><?php echo $row['message']; ?></td>
+                    <td><?php echo $row['noteTime']; ?></td>
                 </tr>
             <?php }?>
         </table>
@@ -112,7 +120,6 @@ th:hover{
 <script>
 var queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
-
 var queries = queryString.split("&");
 var index = queries[0].indexOf("=");
 var nameIn = queries[0].substring(index + 1);
@@ -120,28 +127,8 @@ nameIn = nameIn.replace(/_/g, " ");
 index = queries[1].indexOf("=");
 var speciesIn = queries[1].substring(index + 1);
 speciesIn = speciesIn.replace(/_/g, " ");
-
 $("#name").text("Name: " + nameIn);
 $("#species").text("Species: " + speciesIn);
-
-var type, age, exhibit;
-var details;
-$.ajax({
-    type: "POST",
-    url: "http://localhost/Staff/AnimalDetail.php",
-    data: {name: nameIn, species: speciesIn},
-    success: function(obj) {
-        details = obj;
-    }
-
-
-});
-
-console.log(details);
-
-
-
-
 function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("treatmentTable");
